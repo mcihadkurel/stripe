f57f56045f6cb741ce1fe2847b828409f1db84da
Muhammed Cihad Kurel <mcihadkurel@gmail.com>
2021-03-12 13:51:39 +0100

    Updates

---
+++ precache-manifest.dc73c3c8298b68febd5ebd70aee61009.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "104c5b97c74bdc64410f93d1e32fce02",
+    "url": "/stripe/index.html"
+  },
+  {
+    "revision": "1a11ee0e9661186358df",
+    "url": "/stripe/static/css/main.49ac814e.chunk.css"
+  },
+  {
+    "revision": "9d9ce4078b442a71cc60",
+    "url": "/stripe/static/js/2.e1e77988.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stripe/static/js/2.e1e77988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a11ee0e9661186358df",
+    "url": "/stripe/static/js/main.d0173c9e.chunk.js"
+  },
+  {
+    "revision": "f25d2cafeeda02dc947e",
+    "url": "/stripe/static/js/runtime-main.c732f43d.js"
+  },
+  {
+    "revision": "8596ab4a635cf6b6ea535b15bdc0dc67",
+    "url": "/stripe/static/media/hero.8596ab4a.svg"
+  },
+  {
+    "revision": "1090473dd728fcc17033df70118fa668",
+    "url": "/stripe/static/media/logo.1090473d.svg"
+  },
+  {
+    "revision": "58d7e3d689c0cb8eb365b86046ecdce8",
+    "url": "/stripe/static/media/phone.58d7e3d6.svg"
+  }
+]);
